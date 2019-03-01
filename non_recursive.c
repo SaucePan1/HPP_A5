414abf511f911555d97cbb9ff97f8e3de3207f0a
Bernat Gilboy Sopena <>
2019-03-01 11:44:57 +0100

    made the function non-recurisve, also pthread is working with n of workers = 1, I introduced a bug the pos dif is 0.05 now

--- non_recursive.c
+++ non_recursive.c
@@ -3,13 +3,12 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <pthread.h>
 
 #define POS_X  0
 #define POS_Y 1
 #define MASS 2
 
-double total_acc_x =0;
-double total_acc_y =0;
 const double epsilon_0 = 0.001;
 
 
@@ -29,6 +28,7 @@ typedef struct tree_node{
   struct tree_node *right_down; //Q4 child
   struct tree_node *right_up; //Q1 child
 }node_t;
+
 void print_qtree(node_t* node){
   if(node == NULL){printf("Tree is empty \n"); return;}
 
@@ -235,41 +235,126 @@ void delete_tree(node_t** node){
   return;
 }
 
-void get_acc_on_body(double pos_x, double pos_y, node_t ** node, double theta_max, double G){
-    double x_direction = pos_x - (*node)->cm_x;
-    double y_direction = pos_y - (*node)->cm_y;
-    double dist_to_node = sqrt(x_direction*x_direction + y_direction*y_direction);
-    double tol= 1.0e-10; //
-    if((*node)->right_up == NULL && dist_to_node < tol){
-      return;
-    }
-    else if( (*node)->right_up && (*node)->width/dist_to_node > theta_max ){
-      if((*node)-> right_up->tot_mass>0)
-        get_acc_on_body(pos_x, pos_y, &(*node)->right_up, theta_max, G);
-      if( (*node)-> right_down->tot_mass>0)
-        get_acc_on_body(pos_x, pos_y, &(*node)->right_down, theta_max, G);
-      if((*node)-> left_up->tot_mass>0)
-        get_acc_on_body(pos_x, pos_y, &(*node)->left_up, theta_max, G);
-      if( (*node)-> left_down->tot_mass>0)
-        get_acc_on_body(pos_x, pos_y, &(*node)->left_down, theta_max, G);
-      return;
-    }
-    else{ // it's another leaf / it's an inner node that i can approximate
-      double denominator = (dist_to_node+epsilon_0)*(dist_to_node+epsilon_0)*(dist_to_node+epsilon_0);
-      //add to the global varaibles
-      total_acc_x += G* (*node)->tot_mass*x_direction/denominator;
-      total_acc_y += G* (*node)->tot_mass*y_direction/denominator;
-      return;
-    }
 
+typedef struct thread_arg{
+  int thid;
+  int i1;
+  int i2;
+  double theta_max;
+  double G;
+  double delta_t;
+  double* pos_x;
+  double* pos_y;
+  double* vx;
+  double* vy;
+  node_t* node;
+}arg_t;
 
+void *  worker_get_acc(void* arg){
+  arg_t* myarg = (arg_t*)arg;
+  int i1 = myarg->i1;
+  int i2 = myarg->i2;
+  node_t* root = myarg->node;
+  node_t** node;
+  double G= myarg->G;
+  double theta_max =myarg->theta_max;
+  double total_acc_x=0;
+  double total_acc_y=0;
+
+  printf("We are in worker for thread %d\n", myarg->thid);
+  for(int i=i1; i <i2; i ++){
+    node = &root;
+    //calculate forces
+    while((*node)){
+      // Look if we are on a external node.
+      double x_direction = (myarg->pos_x)[i] - (*node)->cm_x;
+      double y_direction = (myarg->pos_y)[i] - (*node)->cm_y;
+      double dist_to_node = sqrt(x_direction*x_direction + y_direction*y_direction);
+      double tol= 1.0e-10; //
+      if(/*(*node)->right_up == NULL &&*/ dist_to_node < tol){
+        delete_tree(node);
+        node = &root;
+      }
+      // Reminder: The only way we can have a node with one of the childs = NULL
+      // but not the others is because we deleted it already.
+      // Remember that before we only checkd if the frist node i.e RU was null,
+      //that told us if all the otherones were null or not
+      else if((*node)->width/dist_to_node > theta_max ){
+        int exp=  ((*node)->right_up!= NULL) + ((*node)->right_down!= NULL)
+        + ((*node)->left_up!= NULL) + ((*node)->left_down!= NULL);
+        switch (exp) {
+          case 4:
+            //nothing is null so we start with right up
+            //if leaf is empty we delete it and jump to next
+            if((*node)->right_up->tot_mass < 0){
+              delete_tree(&(*node)-> right_up);
+              node = &(*node)->right_down; //we can jump to neighbouring node
+            }else{
+            node = &(*node)->right_up; //if its an internal node, we go deeper
+            }
+            break;
+          case 3:
+            if((*node)->right_down->tot_mass < 0){
+              delete_tree(&(*node)-> right_down);
+              node = &(*node)->left_up;
+            }else{
+              node = &(*node)->right_down;
+            }
+            break;
+          case 2:
+            if((*node)->left_up->tot_mass < 0){
+              delete_tree(&(*node)-> left_up);
+              node = &(*node)->left_down;
+            }else{
+              node = &(*node)->left_up;
+            }
+            break;
+          case 1:
+            if((*node)->left_down->tot_mass < 0){
+              delete_tree(&(*node)->left_down);
+              node = &root;
+            }else{
+              node = &(*node)->left_down;
+            }break;
+          default :
+          {
+            //every child is null so we are on a leaf
+            double denominator;
+            denominator = (dist_to_node+epsilon_0)*(dist_to_node+epsilon_0)
+            *(dist_to_node+epsilon_0);
+            //add to the global varaibles
+            total_acc_x += G* (*node)->tot_mass*x_direction/denominator;
+            total_acc_y += G* (*node)->tot_mass*y_direction/denominator;
+            delete_tree(node);
+            node = &root;
+          } //we need brackets to define scope since we are declaring a variable
+        }
+      }else{ // it's another leaf / it's an inner node that i can approximate
+
+        double denominator;
+        denominator = (dist_to_node+epsilon_0)*(dist_to_node+epsilon_0)*(dist_to_node+epsilon_0);
+        //add to the global varaibles
+        total_acc_x += G* (*node)->tot_mass*x_direction/denominator;
+        total_acc_y += G* (*node)->tot_mass*y_direction/denominator;
+        delete_tree(node);
+        node = &root;
+        }
+      }
+      //printf("Acceleration on %d: (%lf , %lf ) \n", i+1, total_acc_x, total_acc_y);
+      myarg->vx[i] += myarg->delta_t  * total_acc_x;
+      myarg->vy[i] += myarg->delta_t  * total_acc_y;
+      myarg->pos_x[i] += myarg->delta_t * myarg->vx[i];
+      myarg->pos_y[i] += myarg->delta_t * myarg->vy[i];
+      total_acc_x=0;
+      total_acc_y=0;
+    }
+    pthread_exit((void*) myarg->thid);
   }
 
 int main(int argc, char *args[]){
-
-  if (argc!=7){
+  if (argc!=8){
     printf("Invalid number of arguments \n");
-    printf("galsim expects: N filename nsteps delta_t theta_max graphics\n");
+    printf("galsim expects: N filename nsteps delta_t theta_max graphics NUM_THREADS\n");
     return -1;
   }
   clock_t begin = clock();
@@ -280,23 +365,25 @@ int main(int argc, char *args[]){
   character to double, maybe a single cast would suffice */
   const double delta_t = atof(args[4]);
   const double theta_max = atof(args[5]);
+  const int NUM_THREADS = atoi(args[7]);
   const double G = -100/(double)N;
   //Read the file with initial conditions
   FILE *file;
   file = fopen(file_name , "rb");
   /*maybe in this case we could allocate memory for this
   matrix statically*/
-  double **arr = (double **)malloc(N*sizeof(double *));
-  for (int i = 0 ; i<N ; i++){
-    arr[i] = (double*)malloc(4 * sizeof(double));
+  if(file_name == NULL){
+    printf("ERROR reading the file");
   }
 
   /*maybe in this case we could allocate memory for this
   matrix statically*/
+  double *pos_x = (double *)malloc(N*sizeof(double));
+  double *pos_y = (double *)malloc(N*sizeof(double));
+  double *vel_x = (double *)malloc(N*sizeof(double));
+  double *vel_y = (double *)malloc(N*sizeof(double));
   double *ma = (double *)malloc(N*sizeof(double));
   double *bri = (double *)malloc(N*sizeof(double));
-
-
 
   for (int i = 0 ; i<(N) ; i++){
     double x , y , vx , vy , mass , bright;
@@ -306,14 +393,15 @@ int main(int argc, char *args[]){
     fread(&vx , sizeof(double) , 1 ,file);
     fread(&vy , sizeof(double) , 1 ,file);
     fread(&bright, sizeof(double) , 1 ,file);
-    arr[i][0] = (double) x;
-    arr[i][1] = (double) y;
+    pos_x[i] = x;
+    pos_y[i] = y;
     ma[i] = mass;
-    arr[i][2] = (double) vx;
-    arr[i][3] = (double) vy;
+    vel_x[i] = vx;
+    vel_y[i] = vy;
     bri[i] = bright;
   }
   fclose(file);
+  printf("We read the file \n");
   //find powers of two so we only have to do it once
   const int K=200;
   double pow_2[K];
@@ -334,7 +422,6 @@ int main(int argc, char *args[]){
   //printf("Printing tree .. \n");
   //print_qtree(root);
 
-  //Calculate forces
   for (int k = 0 ; k<n_steps ; k++){
     node_t * root= (node_t*)malloc(sizeof(node_t));
     root->depth = 1;
@@ -346,30 +433,60 @@ int main(int argc, char *args[]){
     root->left_up = NULL;
     root->right_up=NULL;
     root->right_down=NULL;
-  //  printf("inserting nodes in the tree...\n");
+    printf("Created root ...\n");
     int id_tar;
+    //Create tree for step K
     for(int i=0; i<N; i++){
       id_tar = i;
-      insert(&root ,arr[i][POS_X], arr[i][POS_Y], ma[i], pow_2, id_tar);
+      insert(&root ,pos_x[i], pos_y[i], ma[i], pow_2, id_tar);
     }
-    for(int i=0; i <N; i ++){
-      get_acc_on_body(arr[i][0], arr[i][1], &root, theta_max, G);
-      
-    //  printf("Acceleration on %d: (%lf , %lf ) \n", i+1, total_acc_x, total_acc_y);
-      arr[i][2] += delta_t  * total_acc_x;
-      arr[i][3] += delta_t  * total_acc_y;
-      arr[i][0] += delta_t * arr[i][2];
-      arr[i][1] += delta_t * arr[i][3];
-      total_acc_x=0;
-      total_acc_y=0;
-    }
-    delete_tree(&root);
-    free(root);
 
+    printf("Inserted nodes ...\n");
+    //Start threads
+    int rc;
+    int t;
+    pthread_t thread[NUM_THREADS];
+    pthread_attr_t attr;
+    /* Initialize thread attr and set to JOINABLE*/
+    pthread_attr_init(&attr); //initializes thread attributes with default values
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //set it to detached
+
+    printf("ceating threads \n");
+    for(t=0; t<NUM_THREADS; t++) {
+       printf("Main: creating thread %d\n", t);
+       //prepare thread arguments
+       arg_t* thread_arg = (arg_t*)malloc(sizeof(arg_t));
+       thread_arg->thid = t;
+       thread_arg->i1 = N/NUM_THREADS*t;
+       thread_arg->i2 = N/NUM_THREADS*(t+1);
+       thread_arg->theta_max = theta_max;
+       thread_arg->G = G;
+       thread_arg->delta_t = delta_t;
+       thread_arg->pos_x = &pos_x[0];
+       thread_arg->pos_y = &pos_y[0];
+       thread_arg->vx = &vel_x[0];
+       thread_arg->vy= &vel_y[0];
+       thread_arg->node = root;
+       rc = pthread_create(&thread[t], &attr, worker_get_acc, thread_arg);
+       if (rc) {
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+          }
+       }
+
+    // MAIN CAN TAKE CARE OF THE REST. (calling worker again)
+    //destroy thread attr and join before deleting tree
+    pthread_attr_destroy(&attr);
+    void* status;
+    for(int t=0; t<NUM_THREADS; t++) {
+       rc = pthread_join(thread[t], &status);
+       if (rc) {
+          printf("ERROR; return code from pthread_join() is %d\n", rc);
+          exit(-1);
+          }
+       printf("Main: completed join with thread %d having a status of %ld\n",t,(long)status);
+      }
   }
-
-
-
 
   FILE * fout = fopen("result.gal", "w+");          //check succesful creation/opening of results file
     if (fout == NULL){
@@ -381,10 +498,10 @@ int main(int argc, char *args[]){
 
 for (int j=0; j<N; j++){
   double x , y , vx , vy;
-  x = (double)arr[j][0];
-  y = (double)arr[j][1];
-  vx = (double)arr[j][2];
-  vy = (double)arr[j][3];
+  x = pos_x[j];
+  y = pos_y[j];
+  vx = vel_x[j];
+  vy = vel_y[j];
   fwrite(&x, sizeof(double), 1, fout);
   fwrite(&y,  sizeof(double), 1, fout);
   fwrite(&ma[j] ,  sizeof(double), 1, fout);
@@ -397,11 +514,10 @@ for (int j=0; j<N; j++){
 // print_file(N,fout);
 fclose(fout);
 
-
-  for ( int i = 0; i<N ; i++){
-    free(arr[i]);
-  }
-  free(arr);
+  free(pos_x);
+  free(pos_y);
+  free(vel_x);
+  free(vel_y);
   free(ma);
   free(bri);
   /*
